797e5a3f43155c2dcdd63092581696b44d2aa63d
Brian Hiatt <brian@openmediafoundation.org>
2015-03-10 08:01:33 -0600

    no drush cc

--- scripts/import-pantheon-database.sh
+++ scripts/import-pantheon-database.sh
@@ -3,5 +3,5 @@
 /usr/local/bin/terminus site backup get --site=$1 --env=dev --element=database --to-directory=/tmp --latest
 database_name=`ls /tmp/*.sql.gz`
 gunzip < $database_name | mysql -u $2 -p$3 $4
-cd /var/www/html
-drush cc all
+#cd /var/www/html
+#drush cc all
