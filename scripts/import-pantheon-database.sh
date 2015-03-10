db171841d11c9966b95fd08b94475578c3479c02
Brian Hiatt <brian@openmediafoundation.org>
2015-03-10 07:53:15 -0600

    improved terminus db fetch

--- scripts/import-pantheon-database.sh
+++ scripts/import-pantheon-database.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
-drush @pantheon.cf.dev sql-dump > /tmp/drupal-database.sql
-sed -i '1d' /tmp/drupal-database.sql
+/usr/local/bin/terminus site backup create --site=$1 --env=dev --element=db
+/usr/local/bin/terminus site backup get --site=$1 --env=dev --element=database --to-directory=/tmp --latest
+database_name=`ls /tmp/*.sql.gz`
+gunzip < $database_name | mysql -u $2 -p$3 $4
 cd /var/www/html
-drush sql-cli < /tmp/drupal-database.sql
 drush cc all
